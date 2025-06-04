f1e82badf7b4a5a7693989dbfd2df661679712fe
23E4245034D <a23.ytp8@g.chuo-u.ac.jp>
2025-06-04 14:36:00 +0900

    Descriptionコンポーネントを定義

---
+++ src/Description.tsx
@@ -0,0 +1,10 @@
+function Description() {
+return (
+    <section>
+    <p><strong>時間割を計画するアプリです</strong><button>詳しく</button></p>
+    <p>※中央大学学生向け</p>
+    </section>
+)
+}
+
+export default Description
